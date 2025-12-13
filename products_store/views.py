cc5ed8a926f26878c35182b85cdc6d5e6ec79ede
DimDeveloper18 <dimash.itcout@gmail.com>
2025-12-13 15:55:47 +0000

    Function created 'tools' with 'comments' variable, and library added for 'comment' model.

--- products_store/views.py
+++ products_store/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render, redirect
+from .models import Comment
 
 # Create your views here.
 
 def index(request):
     return render(request, 'products_store/index.html')
+
+def tools(request):
+    comments = {
+        'comments': Comment.objects.all(),
+    }
+    return render(request, 'products_store/tools.html', comments)
