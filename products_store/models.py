4ed1b98c7e7937b545676c0a3da6a4c28f418822
DimDeveloper18 <dimash.itcout@gmail.com>
2025-12-14 11:39:34 +0000

    Django libraries added ('timezone', 'get_user_model', 'User'), class created for 'Comment'.

--- products_store/models.py
+++ products_store/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Comment(models.Model):
+    comname = models.CharField(max_length=100)
+    comtext = models.TextField()
+    commented_date = models.DateTimeField(default=timezone.now)
+    comwriter = models.ForeignKey(User, on_delete=models.CASCADE)
