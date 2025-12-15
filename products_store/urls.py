89515ba1d54ffee7b1d9680f7fc0cbc1a208e85c
DimDeveloper18 <dimash.itcout@gmail.com>
2025-12-15 12:07:13 +0000

    Paths created for 'login' and 'logout' pages. Django library added 'auth_views'.

--- products_store/urls.py
+++ products_store/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name='products_store-index'),
@@ -7,4 +8,6 @@ urlpatterns = [
     path('contact/', views.contact, name='products_store-contact'),
     path('delivery/', views.delivery, name='products_store-delivery'),
     path('register/', views.reg_form, name='products_store-reg_form'),
+    path('login/', auth_views.LoginView.as_view(template_name='products_store/login.html'), name='products_store-login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='products_store/logout.html'), name='products_store-logout'),
 ]
