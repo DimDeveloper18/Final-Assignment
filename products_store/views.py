47faddbaa33a9a77614be75323a2774e720fcc7f
DimDeveloper18 <dimash.itcout@gmail.com>
2025-12-15 23:04:48 +0000

    Function 'profile_page' created with 'if', 'if else' statement for update details, update form, and save them. Libraries for 'UserUpdateDetailsForm', 'User_profileUpdateForm' added.

--- products_store/views.py
+++ products_store/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Comment
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateDetailsForm, User_profileUpdateForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
@@ -37,3 +37,26 @@ def reg_form(request):
 @login_required
 def basket_page(request):
     return render(request, 'products_store/basket.html')
+
+@login_required
+def profile_page(request):
+    if request.method == 'POST':
+        uud_form = UserUpdateDetailsForm(request.POST, instance=request.user)
+        upu_form = User_profileUpdateForm(request.POST, request.FILES, instance=request.user.user_profile)
+
+        if uud_form.is_valid() and upu_form.is_valid():
+            uud_form.save()
+            upu_form.save()
+            messages.success(request, f'User details successfuly updated!')
+            return redirect('products_store-profile_page')
+    else:
+        uud_form = UserUpdateDetailsForm(instance=request.user)
+        upu_form = User_profileUpdateForm(instance=request.user.user_profile)
+
+
+
+    content = {
+        'uud_form': uud_form,
+        'upu_form': upu_form
+    }
+    return render(request, 'products_store/profile.html')
