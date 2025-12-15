d85488e74a07a72b33f170f431c8eff286513b07
DimDeveloper18 <dimash.itcout@gmail.com>
2025-12-15 00:51:12 +0000

    Function 'reg_form' created with 'if', 'ifelse', statements for form validation and save, also messages function. Django libraries added 'UserRegisterForm' and messages.

--- products_store/views.py
+++ products_store/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Comment
+from .forms import UserRegisterForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -17,3 +19,16 @@ def contact(request):
 
 def delivery(request):
     return render(request, 'products_store/delivery.html')
+
+def reg_form(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Registration completed {username}! Please login.')
+            return redirect('products_store-login')
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'products_store/register.html', {'form':form})
