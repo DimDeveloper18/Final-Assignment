34bd288bac734b029724cf7e6ef2189e87da9d35
DimDeveloper18 <dimash.itcout@gmail.com>
2025-12-16 01:47:30 +0000

    Function 'ready' created.

--- products_store/apps.py
+++ products_store/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class ProductsStoreConfig(AppConfig):
     name = 'products_store'
+    default_auto_field = 'django.db.models.BigAutoField'
+
+    def ready(self):
+        import products_store.signals
