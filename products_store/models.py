a6dfcda20e44c61a046903ece3305f4a5dc999f1
DimDeveloper18 <dimash.itcout@gmail.com>
2025-12-16 01:27:48 +0000

    Library added for 'MediaCloudinaryStorage'.

--- products_store/models.py
+++ products_store/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
+from cloudinary_storage.storage import MediaCloudinaryStorage
 
 # Create your models here.
 
