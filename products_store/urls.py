ab4aa3b63af5b48bae89bd040328631973bf9d72
DimDeveloper18 <dimash.itcout@gmail.com>
2025-12-17 13:16:27 +0000

    Path created for tools page comment list.

--- products_store/urls.py
+++ products_store/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from .views import CommentsList
 
 urlpatterns = [
     path('', views.index, name='products_store-index'),
@@ -13,4 +14,5 @@ urlpatterns = [
     path('basket/', views.basket_page, name='products_store-basket_page'),
     path('profile/', views.profile_page, name='products_store-profile_page'),
     path('comments_view/', views.comments_view, name='products_store-comments-view'),
+    path('tools/', CommentsList.as_view(), name='products_store-tools'),
 ]
