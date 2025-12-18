a63025b806f20078767da1d3ca2da033dc4cf8ae
DimDeveloper18 <dimash.itcout@gmail.com>
2025-12-18 11:58:56 +0000

    Path created for password reset confirm page.

--- com_store/urls.py
+++ com_store/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     path('', include('products_store.urls')),
     path('password-reset/', auth_views.PasswordResetView.as_view(
         template_name='products_store/password_reset.html'), name='password_reset'),
+    path('password-reset-confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(
+        template_name='products_store/password_reset_confirm.html'), name='password_reset_confirm'),
 ]
 
 if settings.DEBUG:
