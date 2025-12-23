1b6a5be94eeed1be9402c0fa0f599db9fd174222
DimDeveloper18 <dimash.itcout@gmail.com>
2025-12-23 11:18:57 +0000

    Settings added for auto fieled default primary key.

--- com_store/settings.py
+++ com_store/settings.py
@@ -122,6 +122,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 CRISPY_FAIL_SILENTLY = not DEBUG
