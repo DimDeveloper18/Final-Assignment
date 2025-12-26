bb58e4947a38487f0b65a75d2abcc8d7997e0c38
DimDeveloper18 <dimash.itcout@gmail.com>
2025-12-26 18:24:55 +0000

    Media library added for cloudinary storage and image param. changed.

--- products_store/models.py
+++ products_store/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.urls import reverse
-# from cloudinary_storage.storage import MediaCloudinaryStorage
+from cloudinary_storage.storage import MediaCloudinaryStorage
 
 User = get_user_model()
 
@@ -23,8 +23,7 @@ class Comment(models.Model):
     
 class User_profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics/')
-    # image = models.ImageField(storage=MediaCloudinaryStorage(), upload_to='profile_pics/')
+    image = models.ImageField(storage=MediaCloudinaryStorage(), upload_to='profile_pics/')
 
     def __str__(self):
         return f'{self.user.username} User_profile'
