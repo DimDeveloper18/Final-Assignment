5543eacd8105ed4bb8a504c8854e117157b5b418
DimDeveloper18 <dimash.itcout@gmail.com>
2026-01-03 13:16:31 +0000

    Product and product type added to admin page.

--- products_store/admin.py
+++ products_store/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import Comment
 from .models import User_profile
+from .models import Product
+from .models import Prod_type
 
 # Register your models here.
 
 admin.site.register(Comment)
 admin.site.register(User_profile)
+admin.site.register(Product)
+admin.site.register(Prod_type)
