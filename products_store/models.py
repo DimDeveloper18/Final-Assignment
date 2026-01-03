370d49ab8128665c05b6575477ae012d27c1925c
DimDeveloper18 <dimash.itcout@gmail.com>
2026-01-03 12:34:49 +0000

    Object models created for 'product type' and 'product'.

--- products_store/models.py
+++ products_store/models.py
@@ -27,3 +27,23 @@ class User_profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} User_profile'
+    
+class Prod_type(models.Model):
+    prod_type_name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.prod_type_name
+    
+class Product(models.Model):
+    prod_name = models.CharField(max_length=50)
+    prod_brand = models.CharField(max_length=25)
+    prod_model = models.CharField(max_length=25)
+    prod_id_name = models.IntegerField(max_length=0)
+    prod_stock = models.CharField(max_length=20, unique=True)
+    stock_date = models.DateTimeField(auto_now_add=True)
+    prod_price = models.DecimalField(max_digits=10, decimal_places=2)
+    prod_type = models.ForeignKey(Prod_type, on_delete=models.RESTRICT, null=True)
+    
+    def __str__(self):
+        return self.prod_name
+
