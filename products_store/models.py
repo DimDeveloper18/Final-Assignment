a38fb134c5e531ab7259cf588fc9d6841b7c85c9
DimDeveloper18 <dimash.itcout@gmail.com>
2026-01-20 12:05:03 +0000

    Product type in products changed to relate products to product category.

--- products_store/models.py
+++ products_store/models.py
@@ -44,7 +44,7 @@ class Product(models.Model):
     prod_stock = models.CharField(max_length=20, unique=True)
     stock_date = models.DateTimeField(auto_now_add=True)
     prod_price = models.DecimalField(max_digits=10, decimal_places=2)
-    prod_type = models.ForeignKey(Product_type, on_delete=models.RESTRICT, null=True)
+    prod_type = models.ForeignKey(Product_type, on_delete=models.CASCADE, related_name="products")
     image = models.ImageField(storage=MediaCloudinaryStorage(), upload_to='product_pics/', null=True, blank=True)
     
     def __str__(self):
@@ -77,7 +77,7 @@ class Product_restock(models.Model):
 
 class Basket(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='order')
-    # order_date = models.DateTimeField(auto_now_add=True)
+    order_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Basket of {self.user.username}"
