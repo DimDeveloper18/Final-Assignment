433ae99b375d62250a7b261cb1fee4a74ed6d5f5
DimDeveloper18 <dimash.itcout@gmail.com>
2026-01-26 00:06:33 +0000

    Product type added to admin dashboard.

--- products_store/admin.py
+++ products_store/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from .models import Comment
 from .models import User_profile
 from .models import Product
-from .models import Prod_type
+from .models import Product_type
 
 # Register your models here.
 
 admin.site.register(Comment)
 admin.site.register(User_profile)
 admin.site.register(Product)
-admin.site.register(Prod_type)
+admin.site.register(Product_type)
